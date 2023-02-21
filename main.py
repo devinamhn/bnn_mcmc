a5235b121dca6858d80d1286b2e1a08ed16e1fd2
devinamhn <devinamhn@gmail.com>
2023-02-21 16:23:36 +0000

    moved prediction loop to hmc.py
    cleaning up

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from torchvision.transforms import InterpolationMode
 from pytorch_lightning.demos.mnist_datamodule import MNIST
 from torch.utils.data import DataLoader, random_split
 import torch.nn.functional as F
-from hmc import HMCsampler
+from hmc import HMCsampler, get_predictions
 
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
@@ -41,63 +41,27 @@ model = MLPhmc(28, 2, 10, [1], 'Gaussian').to(device)
 print(summary(model, input_size=(1, 28, 28)))
 
 n_samples = 10
-step_size = 1e-1
-N_leapfrog = 20
+step_size = 1e-4 #1e-1
+N_leapfrog = 50 #20
+burnin = 3
 
-sampler = HMCsampler(n_samples,step_size, N_leapfrog)
+sampler = HMCsampler(n_samples,step_size, N_leapfrog, burnin)
 
-params = sampler.sample(model, train_loader, device)
+samples = sampler.sample(model, train_loader, device)
 
-with torch.no_grad():
-    accs = []
-    for i, (x_test, y_test) in enumerate(test_loader):
-        x_test, y_test = x_test.to(device), y_test.to(device)
-
-        pred = model.eval(x_test, y_test)
-
-        print(y_test)
-        acc = (pred.mean(dim=0).argmax(dim=-1) == y_test).to(torch.float32).mean()
-
-        accs.append(acc.item())
-        if(i==0):
-            break
-
+accs = get_predictions(model, test_loader, device)
+print(samples)
 print("accuracy", np.mean(accs)*100, "%")
-# log_prob = log_prob_fn(model, train_loader, device)
 
-# mom = momentum(model)
-# params = flatten(model)
-# ham_old = hamiltonian(log_prob, mom)
-# N_leapfrog=20
+params_len = len(samples[0][:])
+#print(samples.reshape((1606, n_samples-burnin)))
+samples = samples.reshape((params_len, n_samples-burnin))
+torch.save(samples,'./results/test/samples.pt')
 
-# params, mom = leapfrog(params, mom, log_prob, N_leapfrog, model)
-# print(params, mom)
+plt.plot(samples[0][:].detach().numpy())
+plt.savefig("./results/test/asample.png")
 
-# log_prob = log_prob_fn(model, train_loader, device)
-
-# #dont need these 2 lines - params, mom will be the new values
-# #mom = momentum(model) 
-# #params = flatten(model)
-
-# ham_new = hamiltonian(log_prob, mom)
-
-# print(ham_old, ham_new, ham_new - ham_old)
-
-# print(ham_new/ham_old)
-
-# rho = min(0., -ham_new +  ham_old)
-# print(rho)
-
-# if(rho>= torch.log(torch.rand(1))):
-#     print("Accept")
-
-#     #see line 1000 in samplers.py
-# else:
-#     print("Reject")
-
-exit()
-
-
+#exit()
 
 # mass = 1.0
 # params_init = params
