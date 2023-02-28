e3d845ff1ae5d70562ea86307a92fcb9900d6a92
devinamhn <devinamhn@gmail.com>
2023-02-28 16:13:48 +0000

    updated

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ print("Device: ",device)
 
 datamodule = MNISTDataModule(128)
 batch = 1
+num_batches= 1
 train_loader = datamodule.train_dataloader()
 validation_loader = datamodule.val_dataloader()
 test_loader = datamodule.test_dataloader()
@@ -37,27 +38,41 @@ test_loader = datamodule.test_dataloader()
 # test_loader = datamodule.test_loader()
 
 #model = MLP(28, 2, 10).to(device)
-model = MLPhmc(28, 2, 10, [1], 'Gaussian').to(device)
+model = MLPhmc(28, 20, 10, [1], 'Gaussian').to(device)
 print(summary(model, input_size=(1, 28, 28)))
 
-n_samples = 10
-step_size = 1e-4 #1e-1
+n_samples = 300
+step_size = 2e-4 #1e-3 #1e-1
 N_leapfrog = 50 #20
-burnin = 3
+burnin = 100
 
-sampler = HMCsampler(n_samples,step_size, N_leapfrog, burnin)
+sampler = HMCsampler(n_samples,step_size, N_leapfrog, burnin, device)
 
-samples = sampler.sample(model, train_loader, device)
+samples, log_like, log_prob = sampler.sample(model, train_loader, num_batches, device)
 
 accs = get_predictions(model, test_loader, device)
-print(samples)
+#print(samples)
 print("accuracy", np.mean(accs)*100, "%")
 
 params_len = len(samples[0][:])
 #print(samples.reshape((1606, n_samples-burnin)))
 samples = samples.reshape((params_len, n_samples-burnin))
 torch.save(samples,'./results/test/samples.pt')
+torch.save(log_like,'./results/test/log_like.pt')
 
+plt.figure(dpi=200)
+plt.plot(log_like.detach().numpy())
+plt.savefig("./results/test/log_like.png")
+
+
+torch.save(log_prob,'./results/test/log_prob.pt')
+
+plt.figure(dpi=200)
+plt.plot(log_prob.detach().numpy())
+plt.savefig("./results/test/log_prob.png")
+
+
+plt.figure(dpi=200)
 plt.plot(samples[0][:].detach().numpy())
 plt.savefig("./results/test/asample.png")
 
